8d0f6c1f31386ca8d0649f842e6b581c8949a1b8
SSS <sagaishere1@gmail.com>
2025-12-01 16:05:58 +0300

    deneme

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ali Başkanim',
       theme: ThemeData(
         // This is the theme of your application.
         //
